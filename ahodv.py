527631e7effb88838f75e4709d0e4d842457e6df
travisjlewis <travis.lewis@students.olin.edu>
2011-12-14 03:44:31 -0500

    Added multiple packet ability, reporting of number of hops from each packet. Num hops is stored in a list, yet to be analyzed.

--- ahodv.py
+++ ahodv.py
@@ -16,6 +16,7 @@ class Ahodv(Algorithm):
         self.__nodes = self.g.nodes()
         self.__cnct_by = {}
         self.__path = deque()
+        self.num_hops = 0
         
         self.g.set_node_state(self.g.root, MeshGraph.SENDING)
         self.g.dest = self.__nodes[randint(0, len(self.__nodes)-1)]
@@ -27,7 +28,6 @@ class Ahodv(Algorithm):
         return True
     
     def next_step(self):
-        
         # If destination node has been found, retrace back path to root
         if len(self.__path) > 0:
             prev_node = self.__cnct_by.get(self.__path[0], None)
@@ -36,7 +36,9 @@ class Ahodv(Algorithm):
                 self.__path.appendleft(prev_node)
         
         # Have nodes propagate back once path has been established
+        i = 0
         for node in self.g.nodes_with_state(MeshGraph.SENDING):
+            i = i + 1
             for adj_node in self.g.neighbors(node):
                 if self.g.get_node_state(adj_node) == MeshGraph.PASSIVE:
                     if adj_node == self.g.dest:
@@ -48,14 +50,30 @@ class Ahodv(Algorithm):
                     self.__cnct_by[adj_node] = node
             self.g.set_node_state(node, MeshGraph.CNCTD)
             
+        print(i)
+            
         # Time out nodes that have been around for too long
         for node in self.g.nodes_with_state(MeshGraph.CNCTD):
             self.g.node[node]['time'] = self.g.node[node].get('time', 0) + 1
             if self.g.node[node]['time'] >= self.timeout:
                 self.g.set_node_state(node, MeshGraph.PASSIVE)
                 self.__cnct_by.pop(node, None)
-                
-        # Still need to include hops number?
-        # Only works for single packet
+        
+        self.num_hops = self.num_hops + 1
+	
+    def get_num_hops(self):
+        return self.num_hops
+        
+    def create_new_packet(self):
+        self.num_hops = 0
+        self.__path.clear()
+        self.g.set_node_state(self.g.root, MeshGraph.SENDING)
+        self.g.dest = self.__nodes[randint(0, len(self.__nodes)-1)]
+        
+        for node in self.g.node:
+            self.g.set_node_state(node, MeshGraph.PASSIVE)
+        
+        self.g.set_node_state(self.g.root, MeshGraph.SENDING)
+        print self.g.dest
          
                     
--- simulation.py
+++ simulation.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from meshgraph import MeshGraph
 from algorithm import Algorithm
 from DSRalgorithm import DSRalgorithm
+from ahodv import Ahodv
 import time
     
     
@@ -23,20 +24,35 @@ class Simulation():
         """
         self.g = g
         self.algo = Algo(g)
+        self.num_hop_list = list()
         if not isinstance(self.algo, Algorithm): 
             raise Exception("Algo must be a subclass of Algorithm")
             
-            
+    def __update_graph(self):
+        """ Runs after each step in the simulation
+            Make the graph display changes as a result of the algorithm
+        """
+        nodes = self.g.nodes(data=True)
+        pos = dict()
+        for node in nodes:
+            pos[node[0]] = node[1]['pos']
+        nx.draw_networkx(self.g, pos, with_labels=True, node_color = [Simulation.__color(node) for node in nodes])
+        plt.draw()
+    
     def start(self):
         """ Start the simulation
             @return: True if the simulation runs successfully
         """
         plt.ion()
         self.__update_graph()
-        while self.algo.has_next_step():
-            self.algo.next_step()
-            self.__update_graph()
+        for i in range(2):
+            while self.algo.has_next_step():
+                self.algo.next_step()
+                self.__update_graph()
+            self.num_hop_list.append(self.algo.get_num_hops())
+            self.algo.create_new_packet()
         plt.show()
+        print(self.num_hop_list)
         return True
     
     @staticmethod
@@ -47,24 +63,8 @@ class Simulation():
             1: "red",
             2: "black",
             3: "green"}[state]
-            
-    
-    
-    def __update_graph(self):
-        """ Runs after each step in the simulation
-            Make the graph display changes as a result of the algorithm
-        """
-        nodes = self.g.nodes(data=True)
-        nx.draw_networkx(self.g, 
-                         pos={node[0]: node[1]['pos'] for node in nodes},
-                         with_labels=True,
-                         node_color = [Simulation.__color(node) for node in nodes]
-                        )
-        plt.draw()
-        time.sleep(1)
-
 
 if __name__ == '__main__':
     g = MeshGraph(n_rows=7, n_cols=7, row_dist=2, col_dist=1, max_offset=0.25)
-    s = Simulation(g, DSRalgorithm)
+    s = Simulation(g, Ahodv)
     s.start()
